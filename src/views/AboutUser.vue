4e67614d5ace7f3c0c77f3bbc7b46a24983a4c87
idx123 <jinhalee020@gmail.com>
2023-09-21 03:09:37 +0900

    v-model, @input, @change, .lazy 사용

--- src/views/AboutUser.vue
+++ src/views/AboutUser.vue
@@ -1,11 +1,32 @@
 <template>
-    <div>
-        <h1>This is About</h1>
+    <h1>This is About</h1>
+    <div class="grid-wrapper">
+        <label for="name">이름</label>
+        <input id="name" type="text" v-model="infos.name" @input="print"/>
+        <label for="age">나이</label>
+        <input id="age" type="number" v-model="infos.age" @change="print"/>
+        <label for="job">직업: {{infos.job}}</label>
+        <input id="job" type="text" v-model.lazy="infos.job"/>
     </div>
 </template>
 <script>
-export default {}
+export default {
+    data(){
+        return{
+            infos: { name: '', age: 0, job: '' },
+        }
+    },
+    methods:{
+        print(){
+            console.log(this.infos);
+        }
+    }
+}
 </script>
 <style>
-    
+    .grid-wrapper{
+        display: grid;
+        width: 50%;
+        margin: 0 auto;
+    }
 </style>
